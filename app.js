321118c4fea493c237f68534f6b7b454d3931bae
Kaique-devSantos <kaiqueads.2023@gmail.com>
2023-12-06 13:46:09 -0300

    criando variaveis, numero secreto, chute e comparando as mesmas

---
+++ app.js
@@ -0,0 +1,8 @@
+alert("boas vindas ao jogo do número secreto");
+let numeroSecreto = 5;
+let chute = prompt('escolha um numero entre 1 e 30');
+
+
+if (chute == numeroSecreto) {
+    console.log('Isso ai você acertou o número secreto é (5)')
+}
