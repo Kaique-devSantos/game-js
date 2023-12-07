2f74ffab3a306557719f40428cb6655395d9a1d5
Kaique-devSantos <kaiqueads.2023@gmail.com>
2023-12-07 09:24:54 -0300

    comecando trabalhar com while

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 alert("boas vindas ao jogo do número secreto");
 let numeroSecreto = 5;
 console.log(numeroSecreto);
+let chute;
 
-let chute = prompt('escolha um numero entre 1 e 7');
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 5');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
 
-
-if (chute == numeroSecreto) {
-    console.log(`Isso ai você acertou o número secreto é  ${numeroSecreto}` )
-} else {
-    if (chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`);
-} else {
-        alert(`O número secreto é maior que ${chute}`);
 }
+ }
 
-}
