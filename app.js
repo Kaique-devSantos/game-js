8fe1fae3ce95049b9430f4eb071b852669a566a3
Kaique-devSantos <kaiqueads.2023@gmail.com>
2023-12-11 17:55:36 -0300

    n.s de forma aleatoria de 1 a 100

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-alert("boas vindas ao jogo do número secreto");
-let numeroSecreto = 5;
+//alert("boas vindas ao jogo do número secreto");
+let numeroSecreto = parseInt(Math.random() *100 + 1);
 console.log(numeroSecreto);
+
 let chute;
 let tentativas = 1;
 
 // enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 5');
+    chute = prompt('Escolha um número entre 1 e 100');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
         break;
