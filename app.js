db669d4f2df93db7fa37dd4373fdab5876b223c9
Kaique-devSantos <kaiqueads.2023@gmail.com>
2023-12-11 16:51:01 -0300

    utilizando break e ajustando resposta para o jogador

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 5');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto que é: ${numeroSecreto} com ${tentativas} tentativas`);
+        break;
+        
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
@@ -19,7 +20,13 @@ while (chute != numeroSecreto) {
         }
 
         tentativas++ ; 
-
 }
  }
 
+ 
+
+if(tentativas > 1){
+    alert(`Isso ai! Você descobriu o número secreto que é: ${numeroSecreto} com ${tentativas} tentativas`);
+}else{
+    alert(`Isso ai! Você descobriu o número secreto que é: ${numeroSecreto} com ${tentativas} tentativa`);
+}
